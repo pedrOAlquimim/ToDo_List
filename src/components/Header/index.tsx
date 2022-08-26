236ebb45bd7966ca101f423898ee66bba575e872
Pedro Alquimim <pedrohalquimim@gmail.com>
2022-08-26 09:20:54 -0300

    Header done

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import todoLogo from '../../assets/Logo.svg';
+import styles from './Header.module.css';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img 
+        src={todoLogo}
+      />
+    </header>
+  )
+}
